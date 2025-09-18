18e82222dd41964710487190843a7b73787da9bc
front2-galaxy-it <94291571+Boskolife@users.noreply.github.com>
2025-09-18 17:32:59 +0300

    fix: change sroll video method

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,25 @@
-// new ScrollyVideo({
-//     scrollyVideoContainer: "scrolly-video",
-//     src: "./video/astarta-fixed.mp4"
-//   });
-
-import { gsap } from 'gsap';
-
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-
-
-gsap.registerPlugin(ScrollTrigger);
-const video = document.getElementById('bgVideo');
-
-video.addEventListener('loadedmetadata', () => {
-  gsap.to(video, {
-    currentTime: video.duration,
-    ease: 'none',
-    scrollTrigger: {
-      start: 'top top',
-      end: 'bottom bottom',
-      scrub: true,
-      pin: true,
-    },
+new ScrollyVideo({
+    scrollyVideoContainer: "scrolly-video",
+    src: "./video/astarta-fixed.mp4"
   });
-});
+
+// import { gsap } from 'gsap';
+
+// import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+
+// gsap.registerPlugin(ScrollTrigger);
+// const video = document.getElementById('bgVideo');
+
+// video.addEventListener('loadedmetadata', () => {
+//   gsap.to(video, {
+//     currentTime: video.duration,
+//     ease: 'none',
+//     scrollTrigger: {
+//       start: 'top top',
+//       end: 'bottom bottom',
+//       scrub: true,
+//       pin: true,
+//     },
+//   });
+// });
