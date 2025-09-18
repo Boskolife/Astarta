ea91c969465098f09bab2f4a692f24359b3d6b4e
front2-galaxy-it <94291571+Boskolife@users.noreply.github.com>
2025-09-18 17:41:26 +0300

    feat: remove gsap

--- src/js/main.js
+++ src/js/main.js
@@ -3,24 +3,3 @@ new ScrollyVideo({
     src: "./video/astarta-fixed-2.mp4"
   });
 
-// import { gsap } from 'gsap';
-
-// import { ScrollTrigger } from 'gsap/ScrollTrigger';
-
-
-// gsap.registerPlugin(ScrollTrigger);
-// const video = document.getElementById('bgVideo');
-
-// video.addEventListener('loadedmetadata', () => {
-//   gsap.to(video, {
-//     currentTime: video.duration,
-//     ease: 'none',
-//     scrollTrigger: {
-//       start: 'top top',
-//       end: 'bottom bottom',
-//       scrub: true,
-//       pin: true,
-//     },
-//   });
-// });
-
