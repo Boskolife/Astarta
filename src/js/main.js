79f0026e41914879859f6a5571befdc9a28691a5
front2-galaxy-it <94291571+Boskolife@users.noreply.github.com>
2025-09-18 17:40:40 +0300

    feat: update video

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 new ScrollyVideo({
     scrollyVideoContainer: "scrolly-video",
-    src: "./video/astarta-fixed.mp4"
+    src: "./video/astarta-fixed-2.mp4"
   });
 
 // import { gsap } from 'gsap';
@@ -23,3 +23,4 @@ new ScrollyVideo({
 //     },
 //   });
 // });
+
