467166b00539feb337914a6be4e6ece22b5f1dea
front2-galaxy-it <94291571+Boskolife@users.noreply.github.com>
2025-09-18 17:21:45 +0300

    add: section content

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,4 @@
-import { gsap } from 'gsap';
-
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-
-gsap.registerPlugin(ScrollTrigger);
-
-const video = document.getElementById('bg-video');
-
-video.addEventListener('loadedmetadata', () => {
-  // Привязываем продолжительность видео к длине скролла
-  gsap.to(video, {
-    currentTime: video.duration,
-    ease: 'none',
-    scrollTrigger: {
-      trigger: 'body',
-      start: 'top top',
-      end: 'bottom bottom',
-      scrub: true, // позволяет управлять временем видео при скролле
-    },
+new ScrollyVideo({
+    scrollyVideoContainer: "scrolly-video",
+    src: "./video/astarta-fixed.mp4"
   });
-});
