d9757540a115b31d9d155c36857ca7ed8cdda169
front2-galaxy-it <94291571+Boskolife@users.noreply.github.com>
2025-09-18 17:54:41 +0300

    feat: opacity section with scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,21 @@
+import { gsap } from "gsap";
+    
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const sections = document.querySelectorAll('.section');
+
+sections.forEach(section => {
+  ScrollTrigger.create({
+    trigger: section,
+    start: "top 30%",    // когда верх секции достигнет 60% от высоты окна
+    end: "bottom 60%",   // когда низ секции пройдет 40% от высоты окна
+    toggleClass: {targets: section, className: "active"},
+    // markers: true // включи, если хочешь видеть позиции триггеров
+  });
+});
+
 new ScrollyVideo({
     scrollyVideoContainer: "scrolly-video",
     src: "./video/astarta-fixed-2.mp4"
